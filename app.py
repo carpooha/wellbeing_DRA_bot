528ee95ecc5ba9b28113aa0921e3f0e0234791a8
carpooha <carpooha@gmail.com>
2026-05-22 22:13:46 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ async def cmd_start(message: types.Message):
     
     # Формируем приветствие
     if announcement:
-        welcome_text = f"☕️ Добро пожаловать в Кофе-Бот!\n\n📢 ОБЪЯВЛЕНИЕ:\n{announcement}\n\n---\nВыберите действие:"
+        welcome_text = f"☕️ Привет! мы тут скидываемся на вскусняшки!!\n\n📢 ОБЪЯВЛЕНИЕ:\n{announcement}\n\n---\nВыберите действие:"
     else:
         welcome_text = "☕️ Добро пожаловать в Кофе-Бот!\n\nВыберите действие:"
     await message.answer(welcome_text, reply_markup=get_main_keyboard())
